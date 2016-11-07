88aa3448d253a8c572ea8a79f9547439184d7d60
君哥哥 <1536642700@qq.com>
2016-11-07 18:20:12 +0800

    buffer.toString()

--- assets/lib/index.js
+++ assets/lib/index.js
@@ -1,12 +1,15 @@
 var http=require('https');
 var url='https://movie.douban.com/top250';
+//var url='http://caipiao.163.com/award/'
 
 http.get(url,function(res){
-    var html='';
+    var html=[];
     res.on('data',function(data){
-        html+=data;
+        html.push(data);
     })
     res.on('end',function(){
+        //html=Buffer.concat(html);
+        html=Buffer.toString(html);
         console.log(html);
     })
 }).on('error',function(error){
