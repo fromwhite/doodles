baf3c271842c9110d19269dc6669152e642ea1f6
君哥哥 <1536642700@qq.com>
2016-11-10 16:44:59 +0800

    修改main结构

--- main.js
+++ main.js
@@ -109,11 +109,6 @@ let server=http.createServer(function(request,response){
     pathHandle(realPath);
 });
 
-//server.listen(port);
-//console.log("http server run in port:"+port);
-
-
-
 // build menu
 const files = fs.readdirSync(base);
 // 对文件夹更改进行排序
@@ -129,7 +124,6 @@ const pagePreFix = '';
 const sourcePrefix = 'https://github.com/shui14/notes/tree/master/';
 
 let html = fs.readFileSync('./index.html').toString();
-//let readme = fs.readFileSync('./README.md').toString();
 
 let ul_html = '<div class="view">';
 let md_value = '| 标题 |  |\n| :-------- | :--------:|\n';
@@ -141,29 +135,26 @@ const mlList = [
 
 // 添加列表内容
 mlList.forEach(function(f) {
-  let npath = path.join(base, f);
-  let array = findHtml(npath);
+    let npath = path.join(base, f);
+    let array = findHtml(npath);
 
   array.sort(function(a, b) {
-    return b[2].mtime - a[2].mtime
+        return b[2].mtime - a[2].mtime
   });
 
   if (array.length > 0) {
-    ul_html += `<p>${f}</p><ul class='main'>`;
+        ul_html += `<p>${f}</p><ul class='main'>`;
 
-    array.forEach(function(p) {
-      const title = /<title>(.*)<\/title>/.test(fs.readFileSync(p[0]).toString()) ? RegExp.$1 : 'Document';
+        array.forEach(function(p) {
+        const title = /<title>(.*)<\/title>/.test(fs.readFileSync(p[0]).toString()) ? RegExp.$1 : 'Document';
 
-      const address = pagePreFix + p[0];
-      const filedir = path.dirname(sourcePrefix + p[0]);
+        const address = pagePreFix + p[0];
+        const filedir = path.dirname(sourcePrefix + p[0]);
 
-      ul_html += `
-          <li>
-            <a href='${p[0]}' target='_blank' class='demo-name' title='效果预览'>${title}</a><a href='${filedir}' class='demo-source' target='_blank' title='点击查看源码'>源码</a>
-          </li>
-      `;
-
-      //md_value += `| [${title}](${address}) | [查看代码](${filedir}) |\r`
+        ul_html += `<li>
+                        <a href='${p[0]}' target='_blank' class='demo-name' title='效果预览'>${title}</a><a href='${filedir}' class='demo-source' target='_blank' title='点击查看源码'>源码</a>
+                    </li>
+        `;
     });
 
     ul_html += '</ul>';
@@ -172,7 +163,6 @@ mlList.forEach(function(f) {
 
 ul_html += '</div>';
 html = html.replace(/(<body>)[\s\S]*?(<\/body>)/, '$1' + ul_html + '$2');
-//readme = readme.replace(/(\[placeholder]:p)[\s\S]*?(\[\/placeholder]:p)/, '$1\n' + md_value + '\n$2');
 
 fs.writeFileSync('./index.html', html);
 //fs.writeFileSync('./README.md', readme);
@@ -197,22 +187,22 @@ copen('http://localhost:4000/');
 
 
 function findHtml(folder_path, collector) {
-  collector = collector || [];
+    collector = collector || [];
 
-  let files = fs.readdirSync(folder_path += '/');
-  let npath, stat;
+    let files = fs.readdirSync(folder_path += '/');
+    let npath, stat;
 
-  files.forEach(function(f) {
+    files.forEach(function(f) {
     npath = folder_path + f;
     stat = fs.lstatSync(npath);
 
     if (stat.isDirectory()) {
-      findHtml(npath, collector);
-      return;
+        findHtml(npath, collector);
+        return;
     }
 
     if (/^[^_].+\.html/.test(f)) {
-      collector.push([npath, f, stat]);
+        collector.push([npath, f, stat]);
     }
   });
 
