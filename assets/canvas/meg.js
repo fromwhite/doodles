755329ee2b2156837997825a7437ea887c0b158f
君哥哥 <shui14@users.noreply.github.com>
2017-01-20 14:22:09 +0800

    Update meg.js

--- assets/canvas/meg.js
+++ assets/canvas/meg.js
@@ -63,6 +63,16 @@
         return this;
     }
     
+    mg.raf = (function(){
+			return window.requestAnimationFrame ||
+                   window.webkitRequestAnimationFrame || 
+                   window.mozRequestAnimationFrame || 
+                   window.oRequestAnimationFrame || 
+                   window.msRequestAnimationFrame || 
+                   function (callback) {window.setTimeout(callback, 1000 / 60); 
+                };
+		})();
+    
     
     
     
