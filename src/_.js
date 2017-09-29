d9ae41b43278a2e002ec3f99ff2f1cdec3f645fe
君哥哥 <vvinc@foxmail.com>
2017-09-29 15:12:22 +0800

    + 增加辅助方法

--- src/_.js
+++ src/_.js
@@ -19,7 +19,7 @@ const raf = window.requestAnimationFrame
   window.setTimeout(callback, 1000 / 60); //每帧1000/60ms
 };
 
-const retinaFy = function retinaFy (canvas) {
+const retinaFy = function (canvas) {
     const height = document.body.clientHeight;
     const width = document.body.clientWidth;
   
@@ -30,7 +30,28 @@ const retinaFy = function retinaFy (canvas) {
     canvas.style.height = `${height}px`;
 }
 
-const log = function log(){
+const log = function (){
     //console.clear();
     console.log.apply(console, arguments);
 }
+
+const queue = function(funcs, scope){
+    (function next() {  
+        if(funcs.length > 0) {  
+            funcs.shift().apply(scope || {}, [next].concat(Array.prototype.slice.call(arguments, 0)));  
+        }  
+  })();
+}
+
+const adler32 = function(str){
+    var MOD = 65521;
+    var a = 1;
+    var b = 0;
+    for (var i = 0; i < data.length >>> 0 ; i++) {
+        a = (a + data.charCodeAt(i)) % MOD;
+        b = (b + a) % MOD;
+    }
+    return a | (b << 16);
+}
+
+export { queue }
--- src/street.js
+++ src/street.js
@@ -1,4 +1,54 @@
-console.log('wss')
+import Stats from 'stats.min'
+import { queue } from '_'
+
+
+//资源加载
+class Loader {
+    constructor(){
+        this._images = [];
+    }
+}
+
+//舞台基类
+class Stage {
+    constructor(options){
+        this.context = options.el.getContext('2d');
+        this.update();
+    }
+    clear() {
+        this.context.clearRect(0, 0, this.width, this.height);
+    }
+    update(){
+        this.clear();
+        this._width = options.el.parentNode.offsetWidth;
+        this._height = options.el.parentNode.offsetHeight;
+        this.dpr = window.devicePixelRatio || 1;
+        this.width = dpr * this._width;
+        this.height = dpr * this._height;
+    }
+    draw(){
+
+    }
+}
+
+//缓存ctx
+
+const stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.right = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild( stats.domElement );
+//stats.update()
+
+const canvas = document.querySelector('#ctx');
+
+window.addEventListener('resize', () => {
+    
+});
+
+                
+
 
 if (module.hot) {
     module.hot.accept();
