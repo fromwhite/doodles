1fc3873e9a718f7a4009bba7a3d28330ea92a689
君哥哥 <vvinc@foxmail.com>
2018-03-23 17:42:52 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,63 @@
-const   path = require('path');
-const   webpack = require('webpack');
-const   ExtractTextPlugin = require('extract-text-webpack-plugin');
-const   HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let DEV = process.env.NODE_ENV === 'DEV'; //开发
 let PROD = process.env.NODE_ENV === 'PROD'; //生产
 
 //项目描述 @_name @_description @template
-const 
-    _name='sprite',
-    _description='webgl 2d精灵动画'
-;
+const
+    _name = 'sprite',
+    _description = 'webgl 2d精灵动画';
 
-let entry = PROD 
-    ?   {
-        sprite:'./src/'+ _name +'.js',
-        stage:['stage']
-    }
-    :   [
-        './src/'+ _name +'.js',
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server'
-    ];
+let entry = PROD ? {
+    sprite: './src/' + _name + '.js',
+    stage: ['stage']
+} : [
+    './src/' + _name + '.js',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server'
+];
 
-let plugins = PROD
-    ?   [
-        new webpack.optimize.UglifyJsPlugin({
-            //comments: true,
-            //mangle: false,
-            compress: {
-                warnings: false
-            }
-        }),
-        //new ExtractTextPlugin('css/[name].css'),
-        new HtmlWebpackPlugin(
-            {
-                filename: "app/"+ _name +".html",
-                title: _name,
-                description:_description,
-                template: 'assets/canvas_tpl.html'
-            }
-        ),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production'),
-            },
-        }),
-        //common
-        new webpack.optimize.CommonsChunkPlugin({
-            name:['stage'],
-            minChunks:Infinity
-        })
-    ]
-    :   [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.DefinePlugin({
-            DEVELOPMENT: JSON.stringify(DEV),
-            PRODUCTION: JSON.stringify(PROD)
-        }),
-        new HtmlWebpackPlugin(
-            {
-                filename: "index.html",
-                title: _name,
-                description:_description,
-                template: 'assets/canvas_tpl.html'
-            }
-        )
-    ];
+let plugins = PROD ? [
+    new webpack.optimize.UglifyJsPlugin({
+        //comments: true,
+        //mangle: false,
+        compress: {
+            warnings: false
+        }
+    }),
+    //new ExtractTextPlugin('css/[name].css'),
+    new HtmlWebpackPlugin({
+        filename: "app/" + _name + ".html",
+        title: _name,
+        description: _description,
+        template: 'assets/canvas_tpl.html'
+    }),
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify('production'),
+        },
+    }),
+    //common
+    new webpack.optimize.CommonsChunkPlugin({
+        name: ['stage'],
+        minChunks: Infinity
+    })
+] : [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+        DEVELOPMENT: JSON.stringify(DEV),
+        PRODUCTION: JSON.stringify(PROD)
+    }),
+    new HtmlWebpackPlugin({
+        filename: "index.html",
+        title: _name,
+        description: _description,
+        template: 'assets/canvas_tpl.html'
+    })
+];
 
 
 
@@ -85,8 +76,7 @@ module.exports = {
     },
     plugins: plugins,
     module: {
-        loaders:[
-            {
+        loaders: [{
                 test: /(\.js$|\.jsx$)/,
                 exclude: '/node_modules/',
                 loader: 'babel-loader',
@@ -107,7 +97,7 @@ module.exports = {
             // }
         ]
     },
-    resolve:{
+    resolve: {
         modules: [
             //"node_modules",
             //path.join(__dirname, "src"),
