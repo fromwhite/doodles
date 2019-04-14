e5241f5cc14a71e108707d3859b9bd8250d910ce
hhh <fatliu@foxmail.com>
2019-04-14 17:47:31 +0800

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,94 +1,90 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-let DEV = process.env.NODE_ENV === 'DEV'; //开发
-let PROD = process.env.NODE_ENV === 'PROD'; //生产
+let DEV = process.env.NODE_ENV === "DEV"; //开发
+let PROD = process.env.NODE_ENV === "PROD"; //生产
 
 // 任务描述 @name @description @template
 const works = {
     webgl2d_stage: {
-        name: 'webgl2d_stage',
-        description: 'webgl2d Stage',
-        template: 'template.html',
-        vendor: 'stage'
+        name: "webgl2d_stage",
+        description: "webgl2d Stage",
+        template: "template.html",
+        vendor: "stage"
     },
     sprite_canvas: {
-        name: 'sprite_canvas',
-        description: 'canvas随机图片',
-        template: 'template.html'
+        name: "sprite_canvas",
+        description: "canvas随机图片",
+        template: "template.html"
     },
     webgl2d: {
         name: "webgl2d",
-        description: 'webgl2d glsl渲染',
-        template: 'template.html',
-        vendor: 'glsl'
+        description: "webgl2d glsl渲染",
+        template: "template.html",
+        vendor: "glsl"
     }
-}
-// 设定本任务 和入口文件 let task = works['sprite_canvas']; let task =
-// works['webgl2d_stage'];
-let task = works['webgl2d']
+};
+
+let task = works["webgl2d"];
 
 let entry = PROD
     ? {
-        // 'sprite': './src/' + task.name + '.js',
-    }
+          // 'sprite': './src/' + task.name + '.js',
+      }
     : [
-        './src/' + task.name + '.js',
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server'
-    ];
+          "./src/" + task.name + ".js",
+          "webpack-dev-server/client?http://localhost:8080",
+          "webpack/hot/only-dev-server"
+      ];
 
 let plugins = PROD
     ? [
-        new webpack
-            .optimize
-            .UglifyJsPlugin({
-                //comments: true, mangle: false,
-                compress: {
-                    warnings: false
-                }
-            }),
-        new HtmlWebpackPlugin({
-            filename: "app/" + task.name + ".html",
-            title: task.name,
-            description: task.description,
-            template: 'assets/' + task.template
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('PROD')
-            }
-        })
-    ]
+          new webpack.optimize.UglifyJsPlugin({
+              //comments: true, mangle: false,
+              compress: {
+                  warnings: false
+              }
+          }),
+          new HtmlWebpackPlugin({
+              filename: "app/" + task.name + ".html",
+              title: task.name,
+              description: task.description,
+              template: "assets/" + task.template
+          }),
+          new webpack.DefinePlugin({
+              "process.env": {
+                  NODE_ENV: JSON.stringify("PROD")
+              }
+          })
+      ]
     : [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.DefinePlugin({
-            DEVELOPMENT: JSON.stringify(DEV),
-            PRODUCTION: JSON.stringify(PROD)
-        }),
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            title: task.name,
-            description: task.description,
-            template: 'assets/' + task.template
-        })
-    ];
+          new webpack.HotModuleReplacementPlugin(),
+          new webpack.DefinePlugin({
+              DEVELOPMENT: JSON.stringify(DEV),
+              PRODUCTION: JSON.stringify(PROD)
+          }),
+          new HtmlWebpackPlugin({
+              filename: "index.html",
+              title: task.name,
+              description: task.description,
+              template: "assets/" + task.template
+          })
+      ];
 
 // 补充 CommonsChunk 依赖
 if (PROD) {
-    entry[task.name] = `./src/${task.name}.js`
+    entry[task.name] = `./src/${task.name}.js`;
     if (task.vendor) {
         entry[task.vendor] = task.vendor;
         plugins.push(
-        // 抽取 webpack loader 剥离 webpackJson 冗余
-        new webpack.optimize.CommonsChunkPlugin({
-            name: [
-                task.name, task.vendor, 'manifest'
-            ],
-            minChunks: Infinity
-        }))
+            // 抽取 webpack loader 剥离 webpackJson 冗余
+            new webpack.optimize.CommonsChunkPlugin({
+                name: [task.name, task.vendor, "manifest"],
+                minChunks: Infinity
+            })
+        );
     }
 }
 
@@ -96,9 +92,9 @@ module.exports = {
     devtool: "source-map",
     entry: entry,
     output: {
-        filename: 'build/[name].js',
+        filename: "build/[name].js",
         path: path.join(__dirname),
-        chunkFilename: "build/[name].js",
+        chunkFilename: "build/[name].js"
         // library:task.vendor, libraryTarget: "umd"
     },
     // externals: {     stage:'stage' },
@@ -107,33 +103,35 @@ module.exports = {
         loaders: [
             {
                 test: /(\.js$|\.jsx$)/,
-                exclude: '/node_modules/',
-                loader: 'babel-loader',
+                exclude: "/node_modules/",
+                loader: "babel-loader",
                 query: {
-                    presets: ['es2015', "stage-3"]
+                    presets: ["es2015", "stage-3"]
                 }
-            }, {
+            },
+            {
                 test: /\.(jpg|gif|png)$/,
-                exclude: '/node_modules/',
+                exclude: "/node_modules/",
                 // loader: 'file-loader'
-                loader: 'url-loader?limit=50000'
-            }, {
+                loader: "url-loader?limit=50000"
+            },
+            {
                 test: /\.css$/,
-                loaders: ['style-loader', 'css-loader']
+                loaders: ["style-loader", "css-loader"]
             }
         ]
     },
     resolve: {
         modules: [
-            path.resolve('./src'),
-            path.resolve('./src/stage'),
-            path.resolve('./node_modules')
+            path.resolve("./src"),
+            path.resolve("./src/stage"),
+            path.resolve("./node_modules")
         ],
         extensions: [".js", ".json", ".jsx", ".css", ".gif"]
     },
     devServer: {
         hot: true,
-        publicPath: '/',
+        publicPath: "/",
         stats: {
             colors: true
         },
